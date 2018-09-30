57651e0d8542e600147708b7befdfebeba9717b8
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-09-30 11:53:00 +0530

    added home and blog view components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  allBlogs = [
+    {
+      blogId: 1,
+      lastModified: '04/04/2018',
+      tags: ['welcome', 'hello'],
+      author: 'Ashish Patel',
+      title: 'Welcome to my blog',
+      body: 'This is my first blog post. Thank you for visiting!',
+      image: 'https://i.iheart.com/v3/re/new_assets/59ef8623219fe106dc1b0659'
+    },
+    {
+      blogId: 2,
+      lastModified: '08/08/2018',
+      tags: [],
+      author: 'Ashish Patel',
+      title: 'Full Stack Web Developer',
+      body: 'Full stack web developer works across front-end, back-end and cloud deployment stacks.',
+      image: 'https://cdn-images-1.medium.com/max/2000/1*qBNlFWQ9G_RPVm7tbZaXOw.jpeg'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
